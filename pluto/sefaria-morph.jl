ad10f1c2366032faa9806ebc3b0ece2a8c613b14
dnsmith.neel@gmail.com <dnsmith.neel@gmail.com>
2024-09-22 21:09:10 -0400

    Work on pairing morph data

--- pluto/sefaria-morph.jl
+++ pluto/sefaria-morph.jl
@@ -92,8 +92,26 @@ end
 # ╔═╡ 41271d9e-ec83-435c-aa91-6b2e8b2e42b6
 strengthen.(strong)
 
+# ╔═╡ 0938fd1f-ff1b-4808-bd3d-c70e26b7fb03
+md"""## Pair entries"""
+
+# ╔═╡ 6b50f52b-a3c2-466a-8551-fa7821068f0e
+bdbarticles = BrownDriverBriggs.bdb(tkn)
+
+# ╔═╡ 77d7993b-7317-4121-b609-8280cf317131
+strong_numbers.(bdbarticles)
+
+# ╔═╡ 6ab07294-f6aa-443c-822e-6e8efec66641
+strengthen.(bdbplus)
+
+# ╔═╡ 64be56eb-99e4-461f-a240-59a746c7ffb6
+dbdplusexample = strengthen(bdbplus[5])
+
+# ╔═╡ 77acb21e-6e6f-4e7c-b7cc-be8407eba534
+dbdplusexample.strong_number
+
 # ╔═╡ Cell order:
-# ╠═68026c63-5cf5-47a5-a318-3440de7d201a
+# ╟─68026c63-5cf5-47a5-a318-3440de7d201a
 # ╟─7a1c8187-4438-4370-ba49-166ffbe0c120
 # ╟─5f8b3fa2-7900-11ef-2c80-c9f7cc113072
 # ╟─0503326e-08ff-4a8a-b646-20bb92822a08
@@ -103,9 +121,15 @@ strengthen.(strong)
 # ╠═52de810e-33c5-453c-b5c4-c72d0a90fc36
 # ╠═ec224a8c-dd33-4d1d-b8d0-659c8954155a
 # ╠═22f283cb-e9bd-4812-beac-c2fa7335667d
-# ╠═160d361f-e7da-4827-b3d7-70f305c71432
+# ╟─160d361f-e7da-4827-b3d7-70f305c71432
 # ╠═4988cf67-2b9b-457c-bd17-1714a3dc17f3
 # ╠═6b7dae10-5fe6-4b9c-a631-6796ed7e0718
 # ╠═41271d9e-ec83-435c-aa91-6b2e8b2e42b6
-# ╠═a3417acd-243b-49fc-bb60-c7b874f6ac3e
-# ╠═9bbfa655-81f0-4885-b71d-da6c08c64451
+# ╟─a3417acd-243b-49fc-bb60-c7b874f6ac3e
+# ╟─9bbfa655-81f0-4885-b71d-da6c08c64451
+# ╟─0938fd1f-ff1b-4808-bd3d-c70e26b7fb03
+# ╠═6b50f52b-a3c2-466a-8551-fa7821068f0e
+# ╠═77d7993b-7317-4121-b609-8280cf317131
+# ╠═6ab07294-f6aa-443c-822e-6e8efec66641
+# ╠═64be56eb-99e4-461f-a240-59a746c7ffb6
+# ╠═77acb21e-6e6f-4e7c-b7cc-be8407eba534
